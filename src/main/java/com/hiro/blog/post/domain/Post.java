d3a9dc64b05adb354c51451d66e658a28d7057d3
hiro <kiryuinh32@gmail.com>
2022-06-24 10:41:13 +0900

    feat: Post entity

---
+++ src/main/java/com/hiro/blog/post/domain/Post.java
@@ -0,0 +1,53 @@
+package com.hiro.blog.post.domain;
+
+
+import com.hiro.blog.post.domain.exception.InvalidWriterDeletePostException;
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Getter
+@Entity
+public class Post {
+
+    @Id
+    @Column(columnDefinition = "varbinary(16)")
+    private UUID id;
+
+    @Column(columnDefinition = "varbinary(16)")
+    private UUID writerId;
+
+    private String writerName;
+
+    private String title;
+
+    private String content;
+
+    protected Post() {
+        this.id = UUID.randomUUID();
+    }
+
+    public Post(UUID writerId, String writerName, String title, String content) {
+        this.id = UUID.randomUUID();
+        this.writerId = writerId;
+        this.writerName = writerName;
+        this.title = title;
+        this.content = content;
+    }
+
+    public void validateWriter(UUID writerId) {
+        if (this.writerId.compareTo(writerId) != 0) {
+            throw new InvalidWriterDeletePostException();
+        }
+    }
+
+    public void modify(UUID writerId, String modifiedTitle, String modifiedContent) {
+        validateWriter(writerId);
+
+        this.title = modifiedTitle;
+        this.content = modifiedContent;
+    }
+}
